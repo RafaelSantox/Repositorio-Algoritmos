2c3b7ad40c46e2c17de866b76d5f34a78f50b457
Rafael <rafael.santos@dcomp.ufs.br>
2026-04-11 10:13:23 -0300

    Implementação de Busca e Inserção Após um Nó

--- Node.java
+++ Node.java
@@ -30,6 +30,31 @@ public class Node {
             head = novo;
         }
 
+        public Node find(Object item){
+            Node atual = head;
+            //TODO: percorra a lista comparando atual.item com 'item'
+            //       use .equals() para comparar objetos
+            //       Retorne 'atual' quando encontrar
+            //       Se chegar ao fim (null), retorne null
+            while (atual != null){
+                if (atual.item.equals(item)){
+                    return atual;
+                }
+                atual = atual.next;
+            }
+            return null;
+        }
+
+        public void insertAfter(Node x, Object item){
+            if (x == null) return;
+            //TODO 1: Crie um novo Node com 'item'
+            //TODO 2: faca novo.next = x.next
+            //TODo 3: faca x.next = novo
+            Node novo = new Node(item);
+            novo.next = x.next;
+            x.next = novo;
+        }
+
         //Percorre a lista e imprime todos os elementos
         public void imprimir(){
             Node atual = head;
@@ -46,6 +71,14 @@ public class Node {
             lista.insertFirst("B");
             lista.insertFirst("A");
             lista.imprimir(); // A -> B -> C -> Null
+            Node  noB = lista.find("B");
+            if (noB != null){
+                lista.insertAfter(noB, "B2");
+            }
+            lista.imprimir(); // A -> B -> B2 -> C -> null
+
+            Node noZ = lista.find("Z");
+            System.out.println("Busca por Z: " + noZ); // null
         }
     }
 }
